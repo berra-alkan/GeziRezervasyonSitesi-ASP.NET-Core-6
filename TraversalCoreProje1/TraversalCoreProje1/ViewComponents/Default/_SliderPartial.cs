968f2ae77472fa2052918f6b004ecadeddcd439a
berra-alkan <berra.nur.alkan@gmail.com>
2024-08-22 20:07:50 +0300

    Ana Sayfa
    Default Index ve View Components, Tur Rotalarının Listelenmesi, Feature Statistics, SubAbout

---
+++ TraversalCoreProje1/TraversalCoreProje1/ViewComponents/Default/_SliderPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje1.ViewComponents.Default
+{
+    public class _SliderPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        { 
+            return View(); 
+        } 
+    }
+}
